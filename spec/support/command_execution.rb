a493d44b52a254e9ce9c269af52e60faad8b8f02
Samuel Giddins <segiddins@segiddins.me>
2017-08-24 15:24:42 -0400

    Pretty-print commad invocations

--- spec/support/command_execution.rb
+++ spec/support/command_execution.rb
@@ -8,7 +8,17 @@ module Spec
     include RSpec::Matchers::Composable
 
     def to_s
-      "$ #{command.strip}"
+      c = Shellwords.shellsplit(command.strip).map {|s| s.include?("\n") ? " \\\n  <<EOS\n#{s.gsub(/^/, "  ").chomp}\nEOS" : Shellwords.shellescape(s) }
+      c = c.reduce("") do |acc, elem|
+        concat = acc + " " + elem
+
+        if concat.lines.last.size >= 100
+          acc + " \\\n  " + elem
+        else
+          concat
+        end
+      end
+      "$ #{c.strip}"
     end
     alias_method :inspect, :to_s
 
